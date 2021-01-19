90c5ee5748155b10a722439539511db817210dec
Sushant <sushantberiwal@gmail.com>
2021-01-19 19:32:08 +0530

    send Request done

--- Lec9_Instagram/activity/backend/app.js
+++ Lec9_Instagram/activity/backend/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const userRouter = require("./router/userRouter");
 const postRouter = require("./router/postRouter");
+const { getUserById, getUserByIdPromisified } = require("./controller/userController");
+const connection = require("./model/db");
 const app = express();
 
 
@@ -21,13 +23,79 @@ app.use("/api/post" , postRouter);
 
 // on the basis of id
 
-// getUserById
+function addInFollowingTable(uid , follow_id , isPublic){
+    return new Promise(function(resolve , reject){
+        let sql;
+        if(isPublic){
+            sql = `INSERT INTO following_table(uid , followId) VALUES('${uid}' , '${follow_id}')`;
+        }
+        else{
+            sql = `INSERT INTO following_table(uid , followId , isAccepted ) VALUES('${uid}' , '${follow_id}' , false )`;
+        }
+        connection.query(sql , function(error , data){
+            if(error){
+                reject(error);
+            }
+            else{
+                resolve(data);
+            }
+        })
+    })
+}
+
+
+function addInFollowerTable( follower_id , uid  ){
+    return new Promise(function(resolve , reject){
+        let sql = `INSERT INTO follower_table(uid , followerId) VALUES('${uid}' , '${follower_id}')`;
+        connection.query(sql , function(error , data){
+            if(error){
+                reject(error);
+            }
+            else{
+                resolve(data);
+            }
+        })
+    })
+}
 
 // send request
-app.post("/api/request" , function(req , res){
+app.post("/api/request" , async function(req , res){
+    try{
+        let {uid , follow_id} = req.body;
+        let user = await getUserByIdPromisified(follow_id);
+        console.log(user);
+        let isPublic = user[0].isPublic;
+        if(isPublic){
+            console.log("inside is Public true");
+            //add in following table with isAccepted = true
+            let followData = await addInFollowingTable(uid , follow_id , true);
+            let followerData = await addInFollowerTable(uid , follow_id);
+            res.json({
+                message:"Request sent and accepted !!",
+                followData,
+                followerData
+            })
+        }
+        else{
+            console.log("inside isPublic false");
+            //add in following table with isAccepted = false
+            let data = await addInFollowingTable(uid , follow_id);
+            console.log(data);
+            res.json({
+                message:"Request Sent and is Pending !!",
+                data
+            })
+        }
+    }
+    catch(error){
+        res.json({
+            message:"failed to send request !!",
+            error
+        })
+    }
+
 
 })
-
 
 // accept a pending request
 app.patch("/api/accept" , function(req , res){
