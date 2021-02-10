e0e3f770e580b4ac52ca736e1859db56d7732878
Sushant <sushantberiwal@gmail.com>
2021-02-10 21:01:20 +0530

    oAuth demo completed

--- Lec9_Instagram/facts/oAuth_Google/app.js
+++ Lec9_Instagram/facts/oAuth_Google/app.js
@@ -3,9 +3,39 @@ const app = require("express")();
 let passport = require("passport");
 let GoogleStrategy = require("passport-google-oauth2").Strategy;
 let { CLIENT_ID, CLIENT_PW } = require("./config/secrets");
-
+let connection = require("../../activity/backend/model/db");
+let cookie = require("cookie-session");
 // static files
 app.use(express.static("public"));
+
+app.use(cookie({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: ["askjdbajsbvfwfbha"],
+}))
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+
+passport.serializeUser( function(user , done){
+    console.log("inside serailize user !!");
+    console.log(user);
+    // console.log(done);
+    done(null , user);
+});
+
+// already cookie was present
+passport.deserializeUser( function(data , done){
+    console.log("Inside deserialize");
+    console.log(data);
+    // console.log(done);
+    done(null , data);
+});
+
 
 // setup passport before use
 passport.use(
@@ -17,14 +47,52 @@ passport.use(
       passReqToCallback: true,
     },
     function (request, accessToken, refreshToken, profile, done) {
-        // console.log("Request" , request);
-        console.log("ACCESSTOKE" , accessToken);
-        // console.log("refresh token" , refreshToken);
+        console.log("ACCESSTOKEN" , accessToken);
         console.log("profile" , profile);
-        // console.log("done" , done);
+        let { email , id , displayName , given_name } = profile;
+        let sql = `SELECT * FROM user_table WHERE email = '${email}'`;
+        // profile => first time user // already signed up
+        connection.query(sql , function(error , data){
+            if(error){
+                done(error);
+            }
+            if(data.length){
+                // user pehle se signed up hain
+                console.log(data);
+                console.log("user already signed up !!");
+                // done
+                done( null , data[0] );
+            }
+            else{
+                // createUser
+                let sql = `INSERT INTO user_table(uid , name , email , username ) VALUES('${id}' , '${displayName}' , '${email}' , '${given_name}')`;
+                connection.query(sql , function(err , data){
+                    if(err){
+                        done(err);
+                    }else{
+                        console.log("User created !!!");
+                        // console.log(data);
+                        //
+                        let sql = `SELECT * FROM user_table WHERE email = '${email}'`;
+                        connection.query(sql , function(error , data){
+                            if(error){
+                                done(error);
+                            }
+                            else{
+                                done(null , data[0]);
+                            }
+                        })
+                    }
+                })
+            }
+        })
+        
+        
     }
   )
 );
+
+
 
 
 
@@ -35,8 +103,24 @@ app.get("/auth/google",  passport.authenticate('google' , {scope:['email' , 'pro
 
 app.get("/auth/callback" ,  passport.authenticate('google')  , function(req , res){
     res.send("USER AUTHENTICATED");
+    console.log(req.user);
 })
 
+
+function checkAuth(req , res , next){
+    if(req.user){
+        // user is logged in
+        next();
+    }else{
+        // user not logged in
+        res.send("YOU ARE NOT LOGGED IN !!!");
+    }   
+
+}
+
+app.get("/profile" , checkAuth , function(req , res){
+    res.send(`WELCOME TO PROFILE PAGE ${req.user.name}`);
+})
 
 
 app.listen(4000, function () {
